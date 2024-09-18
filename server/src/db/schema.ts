aa7b2cb3612bff8e124724ebabb733460309b745
DiegoCrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2024-09-18 20:22:29 -0300

    fix: Corrigir a falta de notNull() e defaultNow() no created_at do schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,22 +1,24 @@
-import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
 
-export const goals = pgTable("goals", {
-  id: serial("id").primaryKey(),
-  createdAt: timestamp("created_at", {
+export const goals = pgTable('goals', {
+  id: serial('id').primaryKey(),
+  createdAt: timestamp('created_at', {
     withTimezone: true,
   })
     .notNull()
     .defaultNow(),
-  title: text("title").notNull(),
-  desiredWeeklyFrequency: integer("desired_weekly_frequency").notNull(),
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
 });
 
-export const goalCompletions = pgTable("goal_completions", {
-  id: serial("id").primaryKey(),
-  createdAt: timestamp("created_at", {
+export const goalCompletions = pgTable('goal_completions', {
+  id: serial('id').primaryKey(),
+  createdAt: timestamp('created_at', {
     withTimezone: true,
-  }),
-  goalId: serial("goal_id")
+  })
+    .notNull()
+    .defaultNow(),
+  goalId: serial('goal_id')
     .references(() => goals.id)
     .notNull(),
 });
